c5150810ac976ea93c888bbae6c6f8838006896e
WGQ <wu_g_q@163.com>
2019-10-08 23:36:01 +0800

    更新图论的拓扑排序算法和贪心算法

--- LeetCode/LeetCode_Graphic.py
+++ LeetCode/LeetCode_Graphic.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: list) -> int:
         """
@@ -40,17 +43,130 @@ class Solution:
         :see https://leetcode-cn.com/explore/interview/card/top-interview-quesitons-in-2018/273/graph-theory/1184/
         """
         # 拓扑排序：统计每个节点的入边数量和出边数量，逐个删除入边为0的节点及其出边，若入边为0的节点全部删除后，仍然存在节点，则存在环
-        # 保存未删除的节点
-        undelete_node_set = set([i for i in range(numCourses)])
-        # 保存无入边的节点
-        non_in_node_stack = []
+        # 统计所有节点的入节点和出节点
+        in_side_list = [set() for i in range(numCourses)]
+        out_side_list = [[] for i in range(numCourses)]
 
-        while len(undelete_node_set) > 0 and len(non_in_node_stack) > 0:
+        for prerequisite in prerequisites:
+            in_side_list[prerequisite[1]].add(prerequisite[0])
+            out_side_list[prerequisite[0]].append(prerequisite[1])
 
+        delete_node_set = set()
+        is_to_continue = True
+        while is_to_continue:
+            is_to_continue = False
 
+            # 遍历所有入节点，删除没有入节点的节点
+            for node in range(numCourses):
+                if len(in_side_list[node]) == 0 and node not in delete_node_set:
+                    is_to_continue = True
 
+                    delete_node_set.add(node)
 
+                    for i in out_side_list[node]:
+                        in_side_list[i].remove(node)
+
+        return len(delete_node_set) == numCourses
+
+    def canFinish2(self, numCourses: int, prerequisites: list) -> bool:
+        """
+        课程表（kahn算法，效率高）
+        :see https://leetcode-cn.com/explore/interview/card/top-interview-quesitons-in-2018/273/graph-theory/1184/
+        """
+        # 拓扑排序：删除所有入度为0的节点及其出边，然后逐个其入度为0的子节点
+        # 统计所有节点的入节点和出节点
+        in_side_count_list = [0 for i in range(numCourses)]
+        out_side_list = [[] for i in range(numCourses)]
+
+        for prerequisite in prerequisites:
+            in_side_count_list[prerequisite[1]] += 1
+            out_side_list[prerequisite[0]].append(prerequisite[1])
+
+        # 存储无入边的节点
+        zero_in_side_node_stack = []
+        for node in range(numCourses):
+            if in_side_count_list[node] == 0:
+                zero_in_side_node_stack.append(node)
+
+        # 拓扑排序的数组
+        check_node_list = []
+
+        # 逐个删除入度为0的节点，并将其入度为0的子节点加入栈中，待所有节点遍历之后，若发现仍有节点未被入栈，则存在环
+        while len(zero_in_side_node_stack) != 0:
+            check_node = zero_in_side_node_stack.pop()
+            check_node_list.append(check_node)
+
+            for sub_node in out_side_list[check_node]:
+                in_side_count_list[sub_node] -= 1
+                if in_side_count_list[sub_node] == 0:
+                    zero_in_side_node_stack.append(sub_node)
+
+        return len(check_node_list) == numCourses
+
+    def findOrder(self, numCourses: int, prerequisites: list) -> list:
+        """
+        课程表II
+        :see https://leetcode-cn.com/explore/interview/card/top-interview-quesitons-in-2018/273/graph-theory/1185/
+        """
+        in_side_count_list = [0 for i in range(numCourses)]
+        out_side_list = [[] for i in range(numCourses)]
+
+        for prerequisite in prerequisites:
+            in_side_count_list[prerequisite[1]] += 1
+            out_side_list[prerequisite[0]].append(prerequisite[1])
+
+        # 存储无入边的节点
+        zero_in_side_node_stack = []
+        for node in range(numCourses):
+            if in_side_count_list[node] == 0:
+                zero_in_side_node_stack.append(node)
+
+        # 拓扑排序的数组
+        check_node_list = []
+
+        # 逐个删除入度为0的节点，并将其入度为0的子节点加入栈中，待所有节点遍历之后，若发现仍有节点未被入栈，则存在环
+        while len(zero_in_side_node_stack) != 0:
+            check_node = zero_in_side_node_stack.pop()
+            check_node_list.insert(0, check_node)
+
+            for sub_node in out_side_list[check_node]:
+                in_side_count_list[sub_node] -= 1
+                if in_side_count_list[sub_node] == 0:
+                    zero_in_side_node_stack.append(sub_node)
+
+        return check_node_list if len(check_node_list) == numCourses else []
+
+    def scheduleCourse(self, courses: list) -> int:
+        """
+        课程表III
+        :see https://leetcode-cn.com/problems/course-schedule-iii/
+        """
+        # 贪心算法：将课程按照持续时长的从短到长和结束时间的先后顺序排列
+        courses.sort(key=lambda x: (x[1], x[0]))
+
+        # 已花费的时间
+        cost_time = 0
+        # 已经上过的课程
+        cost_courses = []
+
+        # 取时间最短且最早结束的课程
+        for course in courses:
+            if len(cost_courses) > 0:
+                top = -cost_courses[0]
+                if top > course[0] and cost_time + course[0] > course[1]:
+                    heapq.heapreplace(cost_courses, -course[0])
+                    cost_time = cost_time - top + course[0]
+                elif cost_time + course[0] <= course[1]:
+                    heapq.heappush(cost_courses, -course[0])
+                    cost_time += course[0]
+            else:
+                heapq.heappush(cost_courses, -course[0])
+                cost_time = course[0]
+
+        return len(cost_courses)
 
 
 if __name__ == '__main__':
-    print(Solution().ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+    # print(Solution().ladderLength("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]))
+    print(Solution().scheduleCourse([[5, 15], [3, 19], [6, 7], [2, 10], [5, 16], [8, 14], [10, 11], [2, 19]]))
+    # print(Solution().scheduleCourse([[5, 5], [4, 6], [2, 6]]))
