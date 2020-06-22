135a9b3c2ae9c25fbd1fd6e04c1582ee559e37fc
WGQ <wu_g_q@163.com>
2020-06-22 20:58:12 +0800

    完成模式匹配的解法

--- LeetCode/LeetCode_String_2.py
+++ LeetCode/LeetCode_String_2.py
@@ -19,23 +19,41 @@ class Solution:
         two_count = len(pattern) - one_count
 
         value_length = len(value)
-        for i in range(value_length + 1):
-            # i 表示 a 字符串的长度，通过 a 字符串的长度计算出 b 字符串的长度
-            str_b_length = (value_length - i * one_count) // two_count if two_count > 0 else 0
-            if i * one_count + str_b_length * two_count != value_length:
-                continue
 
-            str_one = value[:i]
-            str_two = value[i:i + str_b_length] if str_b_length > 0 else '0'
-
-            if str_one == str_two:
-                continue
-            elif pattern.replace('1', str_one).replace('2', str_two) == value:
+        if value_length == 0:
+            if one_count > 0 and two_count > 0:
+                return False
+            else:
                 return True
+
+        # 当第二个字符串的数量为0时，只需要判断第一个字符串替换后是否满足条件即可
+        if two_count == 0:
+            return pattern.replace('1', value[:value_length // one_count]) == value
+
+        for i in range(value_length + 1):
+            # i 表示第一个字符串的长度，通过第一个字符串的长度计算出另一个字符串的长度
+            other_str_length = (value_length - i * one_count) // two_count
+            if i * one_count + other_str_length * two_count != value_length:
+                continue
+
+            # 当另一个字符串的长度为0时，只需要判断第一个字符串即可
+            str_one = value[:i]
+            if i > 0 and other_str_length == 0:
+                return pattern.replace('1', str_one).replace('2', '') == value
+
+            if i == 0:
+                if pattern.replace('1', '').replace('2', value[:other_str_length]) == value:
+                    return True
+            else:
+                # 遍历每个长度为other_str_length的字符串，替换后进行对比
+                for j in range(i, value_length - other_str_length + 1, i):
+                    str_two = value[j:j + other_str_length]
+                    if str_one != str_two and pattern.replace('1', str_one).replace('2', str_two) == value:
+                        return True
 
         return False
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.patternMatching("bbbaa", "cccatat"))
+    print(s.patternMatching("aa", ""))
