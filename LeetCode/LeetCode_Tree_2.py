2df5ac9a4c0ab94b00839c00af918909ab01ed02
WuGQ <wu_g_q@163.com>
2020-09-27 10:45:52 +0800

    完成二叉搜索树的最近公共祖先的解法

--- LeetCode/LeetCode_Tree_2.py
+++ LeetCode/LeetCode_Tree_2.py
@@ -174,28 +174,60 @@ class Solution:
 
         return last.right
 
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+        """
+        235. 二叉搜索树的最近公共祖先
+        :see https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+        """
+        from functools import lru_cache
+
+        @lru_cache(None)
+        def exist(tree_root: TreeNode, node: TreeNode) -> bool:
+            """ 判断node节点是否存在于tree_root作为根节点的子树中 """
+            if not tree_root or not node:
+                return False
+            if tree_root and node and tree_root.val == node.val:
+                return True
+            if tree_root.val > node.val:
+                return exist(tree_root.left, node)
+            else:
+                return exist(tree_root.right, node)
+
+        # if not root or root.val == p.val and (exist(root.left, q) or exist(root.right, q)) or root.val == q.val and (exist(root.left, p) or exist(root.right, p)) or exist(root.left, p) and exist(root.right, q) or exist(root.left, q) and exist(root.right, p):
+        #     return root
+        # return self.lowestCommonAncestor(root.left, p, q) or self.lowestCommonAncestor(root.right, p, q)
+        if root.val == p.val and exist(root, q) or root.val == q.val and exist(root, p):
+            return root
+        if p.val < root.val < q.val or q.val < root.val < p.val:
+            return root
+        if root.val > p.val and root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if root.val < p.val and root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
+
 
 if __name__ == '__main__':
     s = Solution()
 
-    root = TreeNode(4)
+    root = TreeNode(6)
     root.left = TreeNode(2)
-    root.right = TreeNode(5)
+    root.right = TreeNode(8)
     # # #
-    root.left.left = TreeNode(1)
-    root.left.right = TreeNode(3)
+    root.left.left = TreeNode(0)
+    root.left.right = TreeNode(4)
 
     # root.left.left.left = TreeNode(8)
     # root.left.left.right = TreeNode(9)
 
     # #
-    # root.left.right.left = TreeNode(10)
-    # root.left.right.right = TreeNode(11)
+    root.left.right.left = TreeNode(3)
+    root.left.right.right = TreeNode(5)
     #
-    # root.right.left = TreeNode(6)
-    # root.right.right = TreeNode(7)
+    root.right.left = TreeNode(7)
+    root.right.right = TreeNode(9)
 
     # root.right.left.right = TreeNode(7)
 
     # print(s.recoverTree(root))
-    print(s.treeToDoublyList(root))
+    print(s.lowestCommonAncestor(root, TreeNode(0), TreeNode(4)).val)
