989bcb1da86a1ed0fd2423d56fd3304730ccb273
WuGQ <wu_g_q@163.com>
2020-10-06 00:21:17 +0800

    完成K 站中转内最便宜的航班的解法

--- LeetCode/LeetCode_Graphic_2.py
+++ LeetCode/LeetCode_Graphic_2.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class Solution:
 
     def solveSudoku(self, board: list) -> None:
@@ -66,18 +69,60 @@ class Solution:
 
         backtrace(0, 0)
 
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, K: int) -> int:
+        """
+        787. K 站中转内最便宜的航班
+        :see https://leetcode-cn.com/problems/cheapest-flights-within-k-stops/
+        """
+        # 建立邻接表
+        neigh_dict = {}
+        for item in flights:
+            if item[0] not in neigh_dict:
+                neigh_dict[item[0]] = []
+            neigh_dict[item[0]].append(item)
+
+        # dp[i][j] 表示最多经过j次中转，最便宜的价格
+        dp = [[float('inf')] * (K + 2) for _ in range(n)]
+
+        # 初始化
+        for i in range(K + 2):
+            dp[src][i] = 0
+
+        queue = [src]
+        step = 1
+        length = 1
+        while step < K + 2 and queue:
+            node = queue.pop(0)
+            length -= 1
+            # 从node开始遍历下一个节点
+            if node not in neigh_dict:
+                continue
+
+            for item in neigh_dict[node]:
+                # 如果node->item[1]的价格更大，就不需要重新遍历这个item[1]
+                if dp[item[1]][step] < dp[node][step - 1] + item[2]:
+                    continue
+
+                # 将新的节点添加到队列中
+                queue.append(item[1])
+
+                # 更新在step次中转内node->item[1]的最便宜价格
+                for i in range(step, K + 2):
+                    dp[item[1]][i] = min(dp[item[1]][i], dp[node][i - 1] + item[2])
+
+            if length == 0:
+                step += 1
+                length = len(queue)
+
+            # print(node)
+            # for i in dp:
+            #     print(i)
+
+        return dp[dst][-1] if dp[dst][-1] != float('inf') else -1
+
 
 if __name__ == '__main__':
     s = Solution()
-    arr = [["5", "3", ".", ".", "7", ".", ".", ".", "."],
-           ["6", ".", ".", "1", "9", "5", ".", ".", "."],
-           [".", "9", "8", ".", ".", ".", ".", "6", "."],
-           ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
-           ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
-           ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
-           [".", "6", ".", ".", ".", ".", "2", "8", "."],
-           [".", ".", ".", "4", "1", "9", ".", ".", "5"],
-           [".", ".", ".", ".", "8", ".", ".", "7", "9"]]
-    s.solveSudoku(arr)
-    for i in arr:
-        print(i)
+    arr = [[3, 4, 7], [6, 2, 2], [0, 2, 7], [0, 1, 2], [1, 7, 8], [4, 5, 2], [0, 3, 2], [7, 0, 6], [3, 2, 7], [1, 3, 10], [1, 5, 1], [4, 1, 6],
+           [4, 7, 5], [5, 7, 10]]
+    print(s.findCheapestPrice(8, arr, 4, 3, 7))
