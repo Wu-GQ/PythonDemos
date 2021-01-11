f08f1897c2c0e441f7b1f18072800218142c6c0f
WuGQ <wu_g_q@163.com>
2021-01-11 10:10:11 +0800

    1202. 交换字符串中的元素

---
+++ LeetCode/LeetCode_Array_4.py
@@ -0,0 +1,39 @@
+from typing import List
+
+
+class Solution:
+
+    def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
+        """
+        1202. 交换字符串中的元素
+        :see https://leetcode-cn.com/problems/smallest-string-with-swaps/
+        """
+        from LeetCode.Class.UnionFindClass import UnionFindClass
+        union = UnionFindClass(len(s))
+        for item in pairs:
+            union.union(item[0], item[1])
+
+        from collections import defaultdict
+        dic = defaultdict(list)
+        for i in range(len(s)):
+            ch_code = ord(s[i]) - 97
+            father = union.find_parent(i)
+            if not dic[father]:
+                dic[father] = [0] * 26
+            dic[father][ch_code] += 1
+
+        arr = []
+        for i in range(len(s)):
+            father = union.find_parent(i)
+            for j in range(26):
+                if dic[father][j] > 0:
+                    dic[father][j] -= 1
+                    arr.append(chr(j + 97))
+                    break
+
+        return ''.join(arr)
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.smallestStringWithSwaps("dcab", [[0, 3], [1, 2], [0, 2]]))
